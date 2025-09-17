9ec46a166bfa1146b4260aa758d25d8973f68214
Shereen Elshayp <eshereendev@gmail.com>
2025-09-17 11:43:47 +0300

    another fix

--- resources/views/filament/widgets/simple-order-notifier.blade.php
+++ resources/views/filament/widgets/simple-order-notifier.blade.php
@@ -7,6 +7,19 @@
             this.audio.preload = 'auto';
             this.audio.volume = 0.8;
 
+            // Enable audio on first user interaction
+            const enableAudio = () => {
+                this.audio.play().then(() => {
+                    this.audio.pause();
+                    this.audio.currentTime = 0;
+                    console.log('Audio enabled for notifications');
+                }).catch(() => {});
+                document.removeEventListener('click', enableAudio);
+                document.removeEventListener('keydown', enableAudio);
+            };
+            document.addEventListener('click', enableAudio);
+            document.addEventListener('keydown', enableAudio);
+
             // Listen for new order events
             Livewire.on('new-order-created', (data) => {
                 this.handleNewOrder(data[0]);
@@ -108,8 +121,8 @@
             <div class="flex items-center justify-between">
                 <div class="flex items-center space-x-3">
                     <div class="flex-shrink-0">
-                        <div class="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center">
-                            <svg class="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
+                        <div class="w-6 h-6 bg-green-500 rounded-full flex items-center justify-center">
+                            <svg class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                             </svg>
                         </div>
