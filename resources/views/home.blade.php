e63bd6ea014c6d360fba4fa4833231cdaebf21c7
Shereen Elshayp <eshereendev@gmail.com>
2025-09-19 09:38:03 +0300

    change red color to gray

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
                 <h1 class="text-center md:text-left text-4xl md:text-6xl font-bold mb-4 slide-in">WORKFIT</h1>
                 <p class="text-center lg:text-left  text-xl md:text-2xl pr-8 md:pr-0 mb-8 max-w-xl fade-in">Premium activewear designed for performance </p>
                 <div class="flex flex-col sm:flex-row justify-center lg:mx-0 lg:justify-start gap-4 fade-in">
-                    <a href="{{ route('categories.index', 'women') }}" class="bg-white hover:bg-red-600 text-gray-950 hover:text-white font-bold py-3 px-6 transition-colors uppercase text-center w-2/3 lg:w-auto mx-auto lg:mx-0 ">
+                    <a href="{{ route('categories.index', 'women') }}" class="bg-white hover:bg-gray-700 text-gray-950 hover:text-white font-bold py-3 px-6 transition-colors uppercase text-center w-2/3 lg:w-auto mx-auto lg:mx-0 ">
                         SHOP WOMEN
                     </a>
                     <a href="{{ route('categories.index', 'men') }}" class="bg-white hover:bg-gray-400 text-black font-bold py-3 px-6 transition-colors text-center w-2/3 lg:w-auto mx-auto lg:mx-0 ">
