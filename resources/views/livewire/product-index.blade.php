76aafd46a8e1a9270cdc362291b0c2be4f0ced15
Shereen Elshayp <eshereendev@gmail.com>
2025-09-19 09:33:36 +0300

    change red color to gray

--- resources/views/livewire/product-index.blade.php
+++ resources/views/livewire/product-index.blade.php
@@ -176,7 +176,7 @@
                     @if($product->has_variants)
                     <button wire:click="openVariantModal({{ $product->id }})"
                             class="add-to-cart bg-gray-950 text-white p-2 rounded-full hover:bg-gray-100 hover:text-gray-950 border-2 border-gray-950 transition">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5  hover:text-gray-950" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                         </svg>
                     </button>
@@ -186,7 +186,7 @@
                                 wire:loading.attr="disabled"
                                 wire:target="addSimpleProductToCart({{ $product->id }}, 1)"
                                 class="add-to-cart bg-gray-950 border-2  text-white p-2 rounded-full hover:bg-gray-100 hover:text-gray-950 transition">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-gray-950" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                         </svg>
                     </button>
@@ -339,7 +339,7 @@
             <button wire:click="addToCart"
                     wire:loading.attr="disabled"
                     wire:target="addToCart"
-                    class="px-4 py-2 text-white rounded transition-colors {{ $selectedVariant && $quantity > 0 && $selectedVariant->stock > 0 && $quantity <= $selectedVariant->stock ? 'bg-gray-950 border-2 border-gray-950 hover:bg-red-700' : 'bg-gray-400 cursor-not-allowed' }}"
+                    class="px-4 py-2 text-white rounded transition-colors {{ $selectedVariant && $quantity > 0 && $selectedVariant->stock > 0 && $quantity <= $selectedVariant->stock ? 'bg-gray-950 border-2 border-gray-950 hover:bg-gray-100 hover:text-gray-950' : 'bg-gray-400 cursor-not-allowed' }}"
                     {{ !$selectedVariant || $quantity <= 0 || $selectedVariant->stock <= 0 || $quantity > ($selectedVariant ? $selectedVariant->stock : 0) ? 'disabled' : '' }}>
                 <!-- DEBUG: selectedVariant: {{ $selectedVariant ? 'YES' : 'NO' }} -->
                 <!-- DEBUG: quantity: {{ $quantity }} -->
@@ -350,7 +350,7 @@
                 <!-- DEBUG: condition3 (quantity > stock): {{ $quantity > ($selectedVariant ? $selectedVariant->stock : 0) ? 'YES' : 'NO' }} -->
                 <span wire:loading.remove wire:target="addToCart">Add to Cart</span>
                 <span wire:loading wire:target="addToCart">
-                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white hover:text-gray-950 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
