d0db84b641f5253c27a3b4da0d64777c27041013
Shereen Elshayp <eshereendev@gmail.com>
2025-10-07 16:00:03 +0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,7 @@
 <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 <link rel="shortcut icon" href="/favicon.ico" />
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="MyWebSite" />
 <link rel="manifest" href="/site.webmanifest" />
 <!--google fonts-->
 <link rel="preconnect" href="https://fonts.googleapis.com">
